d2f09511708e8e4b36211f5470040b19534cbd47
kameshsr <kameshsr1338@gmail.com>
2024-11-04 13:11:59 +0530

    MOSIP-35611 Added Trailing slash filter
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- resident/resident-service/src/main/java/io/mosip/resident/ResidentBootApplication.java
+++ resident/resident-service/src/main/java/io/mosip/resident/ResidentBootApplication.java
@@ -8,9 +8,9 @@ import io.mosip.kernel.keymanager.hsm.impl.KeyStoreImpl;
 import io.mosip.kernel.pdfgenerator.itext.impl.PDFGeneratorImpl;
 import io.mosip.kernel.templatemanager.velocity.builder.TemplateManagerBuilderImpl;
 import io.mosip.kernel.transliteration.icu4j.impl.TransliterationImpl;
-import io.mosip.kernel.websub.api.client.PublisherClientImpl;
-import io.mosip.kernel.websub.api.client.SubscriberClientImpl;
+import io.mosip.kernel.websub.api.config.WebSubClientConfig;
 import io.mosip.kernel.websub.api.config.publisher.RestTemplateHelper;
+import io.mosip.kernel.websub.api.config.publisher.WebSubPublisherClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -43,8 +43,8 @@ import org.springframework.web.client.RestTemplate;
 		DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
 @Import({TokenIDGenerator.class, ValidateTokenUtil.class, CbeffImpl.class, TransliterationService.class, TransliterationServiceUtil.class
 , PDFGeneratorImpl.class, UinValidatorImpl.class, VidValidatorImpl.class, RidValidatorImpl.class, TemplateManagerBuilderImpl.class,
-		SubscriberClientImpl.class, RestTemplateHelper.class, TransliterationImpl.class, PublisherClientImpl.class, IdObjectValidatorConfig.class
-, RestTemplate.class, KeyStoreImpl.class})
+		RestTemplateHelper.class, TransliterationImpl.class, IdObjectValidatorConfig.class
+, RestTemplate.class, KeyStoreImpl.class, WebSubClientConfig.class, WebSubPublisherClientConfig.class})
 public class ResidentBootApplication {
 
 	public static void main(String[] args) {
